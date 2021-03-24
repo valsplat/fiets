8372d54e47455af24c1e96f937e45989a5d0dc71
Bjorn Post <bjorn.post@gmail.com>
2021-03-24 17:09:47 +0100

    Fix - only flush when there's something to flush

--- src/Fiets/Util/ServerSentEvent.php
+++ src/Fiets/Util/ServerSentEvent.php
@@ -1,25 +1,31 @@
 <?php
-	namespace Fiets\Util;
 
-	class ServerSentEvent {
+namespace Fiets\Util;
 
-		/**
-		 * Constructs the SSE data format and flushes that data to the client.
-		 *
-		 * @param string $msg Line of text that should be transmitted.
-		 * @param string $id Timestamp/id of this connection.
-		 * @author http://www.html5rocks.com/en/tutorials/eventsource/basics/
-		 */
-		public static function send($msg, $id = null) {
-			if(empty($id)) {
-				$id = time();
-			}
+    class ServerSentEvent
+    {
+        /**
+         * Constructs the SSE data format and flushes that data to the client.
+         *
+         * @param string $msg line of text that should be transmitted
+         * @param string $id  timestamp/id of this connection
+         *
+         * @author http://www.html5rocks.com/en/tutorials/eventsource/basics/
+         */
+        public static function send($msg, $id = null)
+        {
+            if (empty($id)) {
+                $id = time();
+            }
 
-			echo "id: $id" . PHP_EOL;
-			echo "data: $msg" . PHP_EOL;
-			echo PHP_EOL;
+            echo "id: $id".PHP_EOL;
+            echo "data: $msg".PHP_EOL;
+            echo PHP_EOL;
 
-			ob_flush();
-			flush();
-		}
-	}
+            if (ob_get_level() > 0) {
+                ob_flush();
+            }
+
+            flush();
+        }
+    }
